6df868bd4840b6f881ae4984d57748fbaa5ae031
Prabath Rathnamalala <prabathrathnamalala2k2@gmail.com>
2025-05-31 02:54:12 +0530

    update proxy

--- broadcast-dashboard/src/components/dashboard/ProxyQueue.js
+++ broadcast-dashboard/src/components/dashboard/ProxyQueue.js
@@ -1,4 +1,30 @@
-import React from 'reac// Clip D with progress bar */}
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { proxyQueueData } from '../../data/mockData';
+
+const ProxyQueue = () => {
+  const queueItems = proxyQueueData;
+  
+  return (
+    <Card className="card-dark fade-in">
+      <Card.Body>
+        <h6 className="section-header">PROXY QUEUE</h6>
+        
+        {/* Clip C with progress bar */}
+        <div className="list-item">
+          <div className="flex-grow-1">
+            <div className="d-flex align-items-center">
+              <span className="status-indicator status-yellow"></span>
+              <div className="list-item-title">{queueItems[0].name}</div>
+            </div>
+            <div className="progress-custom">
+              <div className="progress-bar-custom" style={{ width: `${queueItems[0].progress}%` }}></div>
+            </div>
+          </div>
+          <div className="list-item-meta">{queueItems[0].progress} % ({queueItems[0].resolution})</div>
+        </div>
+        
+        {/* Clip D with progress bar */}
         <div className="list-item">
           <div className="flex-grow-1">
             <div className="d-flex align-items-center">
@@ -24,44 +50,11 @@ import React from 'reac// Clip D with progress bar */}
             </div>
           </div>
           <div className="list-item-meta">{queueItems[2].progress} % ({queueItems[2].resolution})</div>
-        </div>d } from 'react-bootstrap';
-import { proxyQueueData } from '../../data/mockData';
-
-const ProxyQueue = () => {
-  const queueItems = proxyQueueData;
-  
-  return (
-    <Card className="card-dark fade-in">
-      <Card.Body>
-        <h6 className="section-header">PROXY QUEUE</h6>
-        
-        {/* Clip C with progress bar */}
-        <div className="list-item">
-          <div className="flex-grow-1">
-            <div className="list-item-title">{queueItems[0].name}</div>
-            <div className="progress-custom">
-              <div className="progress-bar-custom" style={{ width: `${queueItems[0].progress}%` }}></div>
-            </div>
-          </div>
-          <div className="list-item-meta">{queueItems[0].progress} %</div>
         </div>
         
-        {/* Clip D with status indicator */}
-        <div className="list-item">
-          <div className="d-flex align-items-center">
-            <span className="status-indicator status-green"></span>
-            <div className="list-item-title">{queueItems[1].name}</div>
-          </div>
-          <div className="list-item-meta">{queueItems[1].resolution}</div>
-        </div>
-        
-        {/* News Segment with status indicator */}
-        <div className="list-item">
-          <div className="d-flex align-items-center">
-            <span className="status-indicator status-red"></span>
-            <div className="list-item-title">{queueItems[2].name}</div>
-          </div>
-          <div className="list-item-meta">{queueItems[2].resolution}</div>
+        {/* View All link */}
+        <div className="text-end mt-3">
+          <a href="#" className="view-all-link">VIEW ALL</a>
         </div>
       </Card.Body>
     </Card>
